f224ac11ce8240b9d1a955e7909e4f7ff8f13b09
SakushiPersonal <maranatha.bruno1997@gmail.com>
2023-03-06 17:09:50 -0300

    __str__ method

--- Django_vote_app/polls/models.py
+++ Django_vote_app/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,8 +10,16 @@ class Question(models.Model):
     question_text = models.CharField(max_length=300)
     pub_date = models.DateTimeField('published date')
 
+    def __str__(self) -> str:
+        return self.question_text
+    
+    def was_recently_published(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=300)
     votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
