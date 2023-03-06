225b0736be76456378b8153325550b4f2a3f3b04
SakushiPersonal <maranatha.bruno1997@gmail.com>
2023-03-05 21:07:16 -0300

    migratios choice, question

--- Django_vote_app/polls/models.py
+++ Django_vote_app/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+#not neded to define id, Django creates it automatically
+    question_text = models.CharField(max_length=300)
+    pub_date = models.DateTimeField('published date')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=300)
+    votes = models.IntegerField(default=0)
