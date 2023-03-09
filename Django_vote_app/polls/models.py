667a41e9cd3a161d30e72eeb1035b42052d2748e
SakushiPersonal <maranatha.bruno1997@gmail.com>
2023-03-09 14:44:15 -0300

    Class5: continue

--- Django_vote_app/polls/models.py
+++ Django_vote_app/polls/models.py
@@ -14,7 +14,7 @@ class Question(models.Model):
         return self.question_text
     
     def was_recently_published(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return timezone.now() >=self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- Django_vote_app/polls/tests.py
+++ Django_vote_app/polls/tests.py
@@ -9,8 +9,26 @@ from .models import Question
 #Models or Views
 class QuestionModelTests(TestCase):
 
+    def setUp(self) -> None:
+        self.question = Question(question_text="which game developer company is the best?")
+
+
     def test_was_recentrly_published_with_future_questions(self):
         """was_recently_publshed returns False for questions whose pub_date is in the future"""
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(question_text="which game developer company is the best?", pub_date=time)
-        self.assertIs(future_question.was_recently_published(), False)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_recently_published(), False)
+
+
+    def test_was_recentrly_published_with_present_questions(self):
+        """was_recently_publshed returns True for questions whose pub_date is in the rpesent day"""
+        time = timezone.now() - datetime.timedelta(hours=22)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_recently_published(), True)
+
+
+    def test_was_recentrly_published_with_past_questions(self):
+        """was_recently_publshed returns False for questions whose pub_date is more than a day in the past"""
+        time = timezone.now() - datetime.timedelta(days=2)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_recently_published(), False)
